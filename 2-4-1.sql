f59ff74e6fae149ec082efe7a2e85ca5adb0cfab
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2023-09-26 02:48:37 +0800

    Add files via upload

---
+++ 2-4-1.sql
@@ -0,0 +1,4 @@
+USE MySchoolDB
+INSERT INTO Department
+VALUES ('D001', ' 資工系',' 李春雄'),
+('D002', ' 資管系',' 李碩安')
---
+++ 2-4-2.sql
@@ -0,0 +1,4 @@
+SELECT TOP (1000) [Dep_code]
+      ,[Dep_name]
+      ,[Dep_head]
+  FROM [MySchoolDB].[dbo].[Department]
